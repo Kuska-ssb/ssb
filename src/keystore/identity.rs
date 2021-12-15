e04426524ae559dc1ae1a1cd44ddc26d9e1381db
mycognosist <gnomad@cryptolab.net>
2021-12-15 15:28:00 +0200

    group identity types together

--- src/keystore/identity.rs
+++ src/keystore/identity.rs
@@ -1,7 +1,18 @@
 use crate::crypto::CURVE_ED25519_SUFFIX;
 use kuska_sodiumoxide::crypto::sign::ed25519;
 
-#[derive(Debug, Clone)]
+/// Ed25519 signature scheme identifier.
+pub const CURVE_ED25519: &str = "ed25519";
+
+#[derive(Serialize, Deserialize)]
+pub struct JsonSSBSecret {
+    pub curve: String,
+    pub id: String,
+    pub private: String,
+    pub public: String,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct OwnedIdentity {
     pub id: String,
     pub pk: ed25519::PublicKey,
