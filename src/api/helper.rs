c8d86235c1371f1dbd5fbacbf5d8eaa9a990b5f5
mycognosist <gnomad@cryptolab.net>
2022-02-04 10:06:36 +0200

    add data type and methods to send invite requests

--- src/api/helper.rs
+++ src/api/helper.rs
@@ -1,6 +1,7 @@
 use crate::{
     api::dto::content::{
-        FriendsHops, RelationshipQuery, SubsetQuery, SubsetQueryOptions, TypedMessage,
+        FriendsHops, InviteCreateOptions, RelationshipQuery, SubsetQuery, SubsetQueryOptions,
+        TypedMessage,
     },
     feed::Message,
     rpc::{Body, BodyType, RequestNo, RpcType, RpcWriter},
@@ -13,6 +14,8 @@ const MAX_RPC_BODY_LEN: usize = 65536;
 
 #[derive(Debug)]
 pub enum ApiMethod {
+    InviteCreate,
+    InviteUse,
     FriendsIsFollowing,
     FriendsIsBlocking,
     FriendsHops,
@@ -31,6 +34,8 @@ impl ApiMethod {
     pub fn selector(&self) -> &'static [&'static str] {
         use ApiMethod::*;
         match self {
+            InviteCreate => &["invite", "create"],
+            InviteUse => &["invite", "use"],
             FriendsIsFollowing => &["friends", "isFollowing"],
             FriendsIsBlocking => &["friends", "isBlocking"],
             FriendsHops => &["friends", "hops"],
@@ -48,6 +53,8 @@ impl ApiMethod {
     pub fn from_selector(s: &[&str]) -> Option<Self> {
         use ApiMethod::*;
         match s {
+            ["invite", "create"] => Some(InviteCreate),
+            ["invite", "use"] => Some(InviteUse),
             ["friends", "isFollowing"] => Some(FriendsIsFollowing),
             ["friends", "isBlocking"] => Some(FriendsIsBlocking),
             ["friends", "hops"] => Some(FriendsHops),
@@ -80,6 +87,35 @@ impl<W: Write + Unpin> ApiCaller<W> {
 
     pub fn rpc(&mut self) -> &mut RpcWriter<W> {
         &mut self.rpc
+    }
+
+    /// Send ["invite", "create"] request.
+    pub async fn invite_create_req_send(&mut self, uses: u16) -> Result<RequestNo> {
+        let args = InviteCreateOptions { uses };
+        let req_no = self
+            .rpc
+            .send_request(
+                ApiMethod::InviteCreate.selector(),
+                RpcType::Async,
+                &args,
+                &None::<()>,
+            )
+            .await?;
+        Ok(req_no)
+    }
+
+    /// Send ["invite", "use"] request.
+    pub async fn invite_use_req_send(&mut self, invite_code: &str) -> Result<RequestNo> {
+        let req_no = self
+            .rpc
+            .send_request(
+                ApiMethod::InviteUse.selector(),
+                RpcType::Async,
+                &invite_code,
+                &None::<()>,
+            )
+            .await?;
+        Ok(req_no)
     }
 
     /// Send ["friends", "isFollowing"] request.
