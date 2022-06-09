6b0457a8ddd0e86a8f84cffac2b5cb835723822f
notplants <max@mfowler.info>
2022-06-09 17:07:08 +0200

    Fix formatting

--- src/keystore/mod.rs
+++ src/keystore/mod.rs
@@ -4,6 +4,11 @@ mod identity;
 pub mod patchwork;
 mod util;
 
-pub use gosbot::{from_gosbot_local, read_gosbot_config, write_gosbot_config, from_custom_gosbot_keypath};
+pub use gosbot::{
+    from_custom_gosbot_keypath, from_gosbot_local, read_gosbot_config, write_gosbot_config,
+};
 pub use identity::{JsonSSBSecret, OwnedIdentity, CURVE_ED25519};
-pub use patchwork::{from_patchwork_local, read_patchwork_config, write_patchwork_config, from_custom_patchwork_keypath};
+pub use patchwork::{
+    from_custom_patchwork_keypath, from_patchwork_local, read_patchwork_config,
+    write_patchwork_config,
+};
