63cb26075cc0e38c34d437a8be419ec56ca1fb9c
mycognosist <gnomad@cryptolab.net>
2022-11-18 10:38:58 +0200

    add struct for tangles.thread request options

--- src/api/dto/mod.rs
+++ src/api/dto/mod.rs
@@ -4,6 +4,7 @@ mod error;
 mod history_stream;
 mod latest;
 mod stream;
+mod tangles;
 mod whoami;
 
 pub use blobs::*;
@@ -11,4 +12,5 @@ pub use error::*;
 pub use history_stream::*;
 pub use latest::*;
 pub use stream::*;
+pub use tangles::*;
 pub use whoami::*;
